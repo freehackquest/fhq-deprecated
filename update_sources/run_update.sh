aeba5284960b44752a584c693574e92de36bd483
Eugene Sopov <eso@startext.tomsk.ru>
2013-08-28 11:10:01 +0700

    fixed script

--- update_sources/run_update.sh
+++ update_sources/run_update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # clone or pull data
-if [ -d fhq.github ]; then
-   cd fhq.github
+if [ -d fhq.github.temp ]; then
+   cd fhq.github.temp
    git checkout .
    git pull
 else
-   git clone https://github.com/sea-kg/fhq fhq.github
-   cd fhq.github
+   git clone https://github.com/sea-kg/fhq fhq.github.temp
+   cd fhq.github.temp
 fi
 
 chmod +x update_sources/update.sh
--- update_sources/update.sh
+++ update_sources/update.sh
@@ -6,7 +6,14 @@ echo "pwd = $(pwd)/$1/php/*"
 echo "1 = $1"
 
 # backup
-FILE_TAR_GZ=$(echo "`pwd`/../fhq.autobackups/fhq_php_`date +%Y-%m-%d`.tar.gz");
+BACKUP_DIR="`pwd`/../fhq.autobackups"
+
+if [ ! -d $BACKUP_DIR ]; then
+  mkdir $BACKUP_DIR
+fi
+FILE_TAR_GZ="$BACKUP_DIR/fhq_php_`date +%Y-%m-%d`.tar.gz");
+
+if
 echo "FILE_TAR_GZ = $FILE_TAR_GZ"
 tar -zcvf $FILE_TAR_GZ $1
 
