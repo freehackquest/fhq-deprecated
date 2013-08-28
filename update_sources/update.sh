8154c444916a09caea0f23ee3ecc799acdd2a434
Eugene Sopov <eso@startext.tomsk.ru>
2013-08-28 19:36:07 +0700

    fixed script

--- update_sources/update.sh
+++ update_sources/update.sh
@@ -18,8 +18,27 @@ echo "FILE_TAR_GZ = $FILE_TAR_GZ"
 
 tar -zcvf $FILE_TAR_GZ $1
 
-# copy files
+#copy config
+CONFIG_DIR="`pwd`/../fhq.configfolder"
+echo "CONFIG_DIR = $CONFIG_DIR"
+if [ ! -d $CONFIG_DIR ]; then
+  mkdir $CONFIG_DIR
+fi
+cp -rf "$1/config/*" $CONFIG_DIR
+
+
+# remove old files 
+rm -rf $1
+
+# replacment files
 FILES_PHP="`pwd`/php/fhq/*"
 echo "FILES_PHP = $FILES_PHP"
 cp -rf $FILES_PHP $1
-echo "all"
+
+#copy config comback
+cp -rf $CONFIG_DIR "$1/config/*"
+
+#remove temporary folder
+rm -rf $CONFIG_DIR
+
+echo "completed"
