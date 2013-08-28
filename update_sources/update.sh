25b0c96aa6ddd2a0bb4f405a9e396b656e5a883d
Eugene Sopov <eso@startext.tomsk.ru>
2013-08-28 19:26:05 +0700

    fixed script

--- update_sources/update.sh
+++ update_sources/update.sh
@@ -7,18 +7,19 @@ echo "1 = $1"
 
 # backup
 BACKUP_DIR="`pwd`/../fhq.autobackups"
+echo "BACKUP_DIR = $BACKUP_DIR"
 
 if [ ! -d $BACKUP_DIR ]; then
   mkdir $BACKUP_DIR
 fi
-FILE_TAR_GZ="$BACKUP_DIR/fhq_php_`date +%Y-%m-%d`.tar.gz"
 
-if
+FILE_TAR_GZ="$BACKUP_DIR/fhq_php_`date +%Y-%m-%d`.tar.gz"
 echo "FILE_TAR_GZ = $FILE_TAR_GZ"
+
 tar -zcvf $FILE_TAR_GZ $1
 
 # copy files
 FILES_PHP="`pwd`/php/*"
+echo "FILES_PHP = $FILES_PHP"
 cp -rf $FILES_PHP $1
-
 echo "all"
