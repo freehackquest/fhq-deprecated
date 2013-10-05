e7ec02055813ce5dc60f3acffb449ba45e08a57d
sea-kg <mrseakg@gamail.com>
2013-10-06 05:58:47 +0700

    added opened scoreboard

--- php/fhq/registration.php
+++ php/fhq/registration.php
@@ -21,53 +21,6 @@ if(isset($_GET['email']) && isset($_GET['captcha']))
 			echo "<font color=#ff0000>Registration is denied.</font>";
 		};
 		exit;
-		
-		/*			
-		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			echo "<font color=#ff0000>Invalid e-mail address.</font>";
-			exit;
-		}
-		
-		$username = base64_encode(strtoupper($email));	
-		$db = new fhq_database();
-		$query = "select * from user where username = '$username';";
-		$result = $db->query($query);
-		if( $db->count( $result ) >= 1 )
-		{
-			echo '<font color=#ff0000>This e-mail was already registered.</font>';
-			mysql_free_result($result);
-			exit;
-		};
-		
-		$notactivated = md5(rand().rand());
-		$nickname = "hacker-".substr(md5(rand().rand()), 0, 7);
-		$query = "INSERT user( username, password, nick, role, score ) VALUES ('$username','notactivated$notactivated','$nickname','user', 0);";
-		$result = $db->query($query);
-
-		if($result == '1')
-		{
-			
-			$subject = "Activation your account on FreeHackQuest.";
-			
-			$message = "
-Registration:
-
-If you was not tryed register on ".$config['httpname'].", just ignore this mail.
-
-For activate your account, please visit this page:
-".$config['httpname']."registration.php?foractivate=$notactivated
-";
-			$mail = new fhq_mail();
-			$error = "";
-			if( $mail->send($email, $subject, $message, $error) )
-				echo "Check your your e-mail (also check spam).";
-			else
-				echo '<font color=#ff0000>Problem with sending email. '.$error.'</font>';
-			exit;
-		};
-		echo "<font color=#ff0000>Registration is denied.</font>";
-		exit;
-		* */
 };
 
 // ---------------------------------------------------------------------
---
+++ php/fhq/scoreboard.php
@@ -0,0 +1,42 @@
+<?
+	include_once "engine/fhq.php";
+	
+	
+	class fhq_page_scoreboard
+	{
+		function title()
+		{
+			return 'Scoreboard';
+		}
+
+		function echo_head()
+		{
+			echo '';
+		}
+		
+		function echo_onBodyEnd() {
+			echo '';
+		}
+		
+		function echo_content()
+		{
+			$db = new fhq_database();
+			$query = "SELECT iduser, score, nick FROM user WHERE role='user' ORDER BY score DESC";
+
+			$result = $db->query( $query );
+			$i = 1;
+			echo "<br>";
+			while ($row = mysql_fetch_row($result, MYSQL_ASSOC)) // Data
+			{      
+				$nick = $row["nick"];
+				$score = $row["score"];
+				echo ($i++)."<font size=3> $nick (score: $score);</font><br>\n";
+			}
+			mysql_free_result($result);
+		}
+	};
+
+	echo_shortpage(new fhq_page_scoreboard());
+
+	exit;
+?>
