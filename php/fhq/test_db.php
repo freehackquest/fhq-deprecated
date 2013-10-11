0bd31d1d8654d07f6e121d1c8a1c4777106139b7
sea-kg <mrseakg@gamail.com>
2013-10-12 00:11:03 +0700

    fixed test_db

--- php/fhq/test_db.php
+++ php/fhq/test_db.php
@@ -1,8 +1,23 @@
 <?
-  exit;
+	exit;
 	include_once "config/config.php";
+	include_once "engine/fhq.php";
 	$db = new fhq_database();
 	$db->connect($config);
-	$result = $db->query("select * from user");
-	print($result);
+	
+	// print($result);
+	
+	{
+		$answer_list = new fhq_answer_list();
+		
+		$result = $db->query("select iduser, idquest from tryanswer where passed = 'Yes'");
+		while ($row = mysql_fetch_row($result, MYSQL_ASSOC)) // Data
+		{   
+			$iduser = $row['iduser'];
+			$idquest = $row['idquest'];
+			echo $iduser." ".$idquest."<br>";
+			$answer_list->movedToBackup($iduser, $idquest);
+		}
+		mysql_free_result($result);
+	}
 ?>
