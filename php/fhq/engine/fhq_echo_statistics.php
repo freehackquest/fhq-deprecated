8072567cd90d73e1bfa6916b76d9808e4a047315
sea-kg <mrseakg@gamail.com>
2013-10-13 22:50:49 +0700

    fixed statistics

--- php/fhq/engine/fhq_echo_statistics.php
+++ php/fhq/engine/fhq_echo_statistics.php
@@ -54,6 +54,7 @@ ORDER BY
 		<th>Score(Min Score)</th>
 		<th>Attempts(Yes)</th>
 		<th>Attempts(No)</th>
+		<th>Users who passed</th>
 	</tr>';
 
 	function getSpec_SQL($table, $idquest, $passed)
@@ -84,7 +85,7 @@ ORDER BY
 		
 		$plus = 0;
 		$minus = 0;
-		
+		$users = '';
 		{
 			$rs = $db->query( getSpec_SQL('tryanswer', $idquest, 'Yes') );
 			$plus += mysql_result($rs, 0, 'cnt');
@@ -103,12 +104,38 @@ ORDER BY
 			mysql_free_result($rs);
 		}
 
+		{
+			$rs = $db->query( '
+				select 
+					t0.iduser, 
+					t0.nick, 
+					t0.username 
+				from 
+					user t0
+				inner join userquest t1 on t0.iduser = t1.iduser 
+				where
+					t0.role = \'user\'
+					and t1.idquest = '.$idquest.'
+					and t1.stopdate <> \'0000-00-00 00:00:00\''
+			);
+			while ($row2 = mysql_fetch_row($rs, MYSQL_ASSOC)) // Data
+			{   
+				//$class = !$class;
+				//$username = base64_decode($row2['username']);
+				$nick = $row2['nick'];
+				//$iduser = $row2['iduser'];
+				$users .= '['.$nick.'] ';
+			}
+			mysql_free_result($rs);
+		}
+		
 		echo ' 
 			<tr '.($class ? 'class="alt"' : '').' >
 				<td><a href=main.php?content_page=view_quest&id='.$idquest.'>'.$idquest.', '.$name.'</a></td>
 				<td>+'.$score.' ( >='.$min_score.') </td>
 				<td>'.$plus.'</td>
 				<td>'.$minus.'</td>
+				<td>'.$users.'</td>
 			</tr>';
 		echo '</pre>';
 	}
