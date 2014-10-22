8bd8fc6f28b386758c15a7a6c6dbdc33cb29160d
sea-kg <mrseakg@gmail.com>
2014-10-22 17:41:18 +0700

    Update api/index.php (include yii framework)

--- php/fhq/api/index.php
+++ php/fhq/api/index.php
@@ -1,3 +1,13 @@
 <?
-	header ("Location: ../");
-	exit;
+// Debug?
+define('YII_DEBUG',true);
+
+$dir = dirname(__FILE__);
+$yii = $dir.'/framework/yii.php';
+$config = $dir.'/protected/config/main.php';
+
+// include Yii bootstrap file
+require_once($yii);
+
+// create a Web application instance and run
+Yii::createWebApplication($config)->run();
