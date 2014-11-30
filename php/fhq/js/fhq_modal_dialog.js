724e93bb4b51759ab87cc935af3e6b8c6a865115
sea-kg <mrseakg@gamail.com>
2014-11-30 10:55:11 +0600

    added hotkey Esc on modal dialog

--- php/fhq/js/fhq_modal_dialog.js
+++ php/fhq/js/fhq_modal_dialog.js
@@ -6,13 +6,16 @@ function showModalDialog(content)
 	document.getElementById('modal_dialog_content').innerHTML = content;
 	document.documentElement.style.overflow = 'hidden';  // firefox, chrome
     document.body.scroll = "no"; // ie only
+    document.onkeydown = function(evt) {
+		if (evt.keyCode == 27)
+			closeModalDialog();
+	}
 }
-   
-    
 
 function closeModalDialog()
 {
 	document.getElementById('modal_dialog').style.visibility = 'hidden';
 	document.documentElement.style.overflow = 'auto';  // firefox, chrome
     document.body.scroll = "yes"; // ie only
+    document.onkeydown = null;
 }
