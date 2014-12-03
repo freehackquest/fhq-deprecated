dc435eecc032810b844d54d2628d58f74e716a4c
Eugene Sopov <eso@startext.tomsk.ru>
2014-12-03 18:24:04 +0600

    redesign games/insert.php

--- php/fhq/api/games/insert.php
+++ php/fhq/api/games/insert.php
@@ -14,46 +14,46 @@ $result = array(
 
 $conn = createConnection($config);
 
-if ( issetParam('global_id') 
-  && issetParam('game_name') 
-  && issetParam('game_logo') 
-  && issetParam('game_type')
-  && issetParam('start_date') 
-  && issetParam('end_date') 
-  && issetParam('author_id') 
-) {
-	$global_id = getParam('global_id', 'none');
-  $game_name = getParam('game_name', 'Unknown');
-  $game_logo = getParam('game_logo', '');
-  $game_type = getParam('game_type', 'jeopardy');
-  $start_date = getParam('start_date', '0000-00-00 00:00:00');
-  $end_date = getParam('end_date', '0000-00-00 00:00:00');
-  $author_id = getParam('author_id', $security->userId());
+if($security->isAdmin())
+  showerror(746, 'Error 746: access denie. you must be admin.');
 
-  if (!is_numeric($author_id))
-		showerror(745, 'Error 745: incorrect author_id');
+$columns = array(
+  'global_id' => 'none',
+  'game_name' => 'Unknown',
+  'game_logo' => '',  
+  'game_type' => 'jeopardy',
+  'start_date' => '0000-00-00 00:00:00',
+  'end_date' => '0000-00-00 00:00:00',
+  'author_id' => $security->userId(),
+);
 
-  if($security->isAdmin())
-  	showerror(746, 'Error 746: access denie. you must be admin.');
+$param_values = array(); 
+$values_q = array();
 
-   $author_id = intval($author_id);
-	
-	try {
-    $query = '
-			INSERT INTO games(global_id, game_name, game_logo, start_date, end_date, change_date, author_id, change_date) 
-      VALUES(?,?,?,?,?,?,?,NOW());
-    ';
-
-		$values = array($global_id, $game_name, $game_logo,$game_type,$start_date,$end_date,$author_id);
-
-		$stmt = $conn->prepare($query);
-		$stmt->execute($values);    
-    $result['data']['game']['id'] = $conn->lastInsertId();
-		$result['result'] = 'ok';
-	} catch(PDOException $e) {
-		showerror(747, 'Error 747: ' + $e->getMessage());
-	}
-} else {
-	showerror(748, 'Error 748: not found parameter id');
+foreach ( $columns as $k => $v) {
+  $values_q[] = '?';
+  if (issetParam($k))
+    $param_values[$k] = getParam($k, $v);
+  else
+    showerror(748, 'Error 748: not found parameter "'.$k.'"');
 }
+
+if (!is_numeric($param_values['author_id']))
+	showerror(745, 'Error 745: incorrect author_id');
+
+$param_values['author_id'] = intval($param_values['author_id']);
+
+$query = 'INSERT INTO games('.implode(',', array_keys($param_values)).', change_date) 
+  VALUES('.implode(',', $values_q).', NOW());';
+
+try {
+	$stmt = $conn->prepare(array_values($param_values));
+	$stmt->execute($values);    
+  $result['data']['game']['id'] = $conn->lastInsertId();
+  $result['result'] = 'ok';
+} catch(PDOException $e) {
+  showerror(747, 'Error 747: ' + $e->getMessage());
+}
+
+
 echo json_encode($result);
