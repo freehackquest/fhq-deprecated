ba63024669ace9f72046a13156e585380c90f1c6
sea-kg <mrseakg@gamail.com>
2015-01-10 18:12:38 +0600

    fixed copyright's

--- php/fhq/about.php
+++ php/fhq/about.php
@@ -3,25 +3,25 @@
 	
 <h1>free-hack-quest</h1>
 This is an open source platform for competitions in computer security.<br>
-Copyright (c) 2011-2014 sea-kg.<br>
+Copyright (c) 2011-2015 sea-kg.<br>
 
 <h1></h1>
 if you wish public your task please send me email with 7z archiv to mrseakg@gmail.com with structure:
 
 topic/score/other/ - files for creating task and for script with solution
 topic/score/files/ - files for tasks
+topic/score/solve_files/ - files for solve
 topic/score/task - description and name
 topic/score/answer - answer for task
 topic/score/author - creator
-topic/score/README - for details
 topic/score/solve - real (and possible) solution by author
 
 <h2>developers</h2>
-Evgenii Sopov, Dmitrii Mukovkin<br>
+Evgenii Sopov, Dmitrii Mukovkin, Igor Polyakov<br>
 <h2>designers</h2>
-Evgenii Sopov, Taisiya Lebedeva<br>
+Evgenii Sopov<br>
 <h2>thanks for</h2>
-Team Keva, Alexey Gulyaev, Alexander Menschikov, Ilya Bokov<br>
+Team Keva, Alexey Gulyaev, Alexander Menschikov, Ilya Bokov, Taisiya Lebedeva<br>
 
 <h1>contacts</h1>
 see us in <a href="http://vk.com/freehackquest">VKontakte</a><br>
--- php/fhq/copyright.php
+++ php/fhq/copyright.php
@@ -1,5 +1,5 @@
 <?php
 echo '<center>
-	<font face="Arial" size=2>Copyright © 2011-2014 sea-kg.</font>
+	<font face="Arial" size=2>Copyright © 2011-2015 sea-kg.</font>
 	<!-- pre><div id="debug_info"/></pre -->
 </center>';
