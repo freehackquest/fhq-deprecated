478bde7e2334fb6003a6e403f8ffc76163b93cd9
sea-kg <mrseakg@gamail.com>
2015-01-14 00:06:12 +0600

    just added token (it do not work now but in future may be do)

--- php/fhq/api/api.lib/api.helpers.php
+++ php/fhq/api/api.lib/api.helpers.php
@@ -107,5 +107,17 @@ class FHQHelpers {
 			$score = $row['sum_score'];
 		return $score;
 	}
+	
+	static function gen_guid() {
+		mt_srand((double)microtime()*10000);//optional for php 4.2.0 and up.
+		$charid = strtoupper(md5(uniqid(rand(), true)));
+		$hyphen = chr(45);// "-"
+		$uuid = substr($charid, 0, 8).$hyphen
+				.substr($charid, 8, 4).$hyphen
+				.substr($charid,12, 4).$hyphen
+				.substr($charid,16, 4).$hyphen
+				.substr($charid,20,12);
+		return $uuid;	
+	}
 }
 
