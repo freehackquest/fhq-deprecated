f648be7f042dd918c4849464d4fc2003d9894961
Eugene Sopov <eso@startext.tomsk.ru>
2015-01-19 15:16:01 +0600

    added column state and form to games

--- php/fhq/api/updates/updates/u0014.php
+++ php/fhq/api/updates/updates/u0014.php
@@ -2,11 +2,13 @@
 
 $updates['u0014'] = array(
 	'to_version' => 'u0015',
-	'name' => 'added column',
-	'description' => 'added column organizators to games',
+	'name' => 'added columns',
+	'description' => 'added column organizators, state and form to games',
 );
 
 function update_u0014($conn) {
-	$conn->prepare('ALTER TABLE `organizators` ADD COLUMN `games` VARCHAR(255) DEFAULT "";')->execute();
+	$conn->prepare('ALTER TABLE `games` ADD COLUMN `organizators` VARCHAR(255) DEFAULT "";')->execute();
+  $conn->prepare('ALTER TABLE `games` ADD COLUMN `state` VARCHAR(255) DEFAULT "copy";')->execute();
+  $conn->prepare('ALTER TABLE `games` ADD COLUMN `form` VARCHAR(255) DEFAULT "online";')->execute();
 	return true;
 }
