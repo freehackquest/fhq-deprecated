a0675347b6a47daaa0f4b5da9f4c44e4aa7e2db1
sea-kg <mrseakg@gamail.com>
2015-03-21 03:43:48 +0600

    redesign updater

--- backend/src/database/databaseupdater.cpp
+++ backend/src/database/databaseupdater.cpp
@@ -86,38 +86,6 @@ void DatabaseUpdater::create(GlobalContext *pGlobalContext) {
 
 // --------------------------------------------------------------------
 
-void DatabaseUpdater::update(GlobalContext *pGlobalContext) {
-	std::cout << " * Update database\n";
-	AutoDatabaseConnection autodb(pGlobalContext);
-	if (autodb.isEmpty()) {
-		std::cerr << "[ERROR] could not connect to database \n";
-		return;
-	}
-
-	int nInstalledUpdates = 0;
-	if ( !autodb.db()->tables().contains( QLatin1String("backend_dbupdates"))) {
-		update0000(autodb.db());
-		nInstalledUpdates++;
-	};
-		
-	if (getLastUpdate(autodb.db()) == "u0000") {
-		update0001(autodb.db());
-		nInstalledUpdates++;
-	};
-
-	if (getLastUpdate(autodb.db()) == "u0001") {
-		update0002(autodb.db());
-		nInstalledUpdates++;
-	};
-	
-	if (getLastUpdate(autodb.db()) == "u0002") {
-		update0003(autodb.db());
-		nInstalledUpdates++;
-	};
-	std::cout << " *** Installed " << nInstalledUpdates << " updates. Current version: " << getLastUpdate(autodb.db()).toStdString() << "\n";
-};
-
-// --------------------------------------------------------------------
 
 void DatabaseUpdater::insertUpdate(QSqlDatabase *db, QString name, QString version) {
 	QSqlQuery query(*db);
@@ -142,7 +110,7 @@ QString DatabaseUpdater::getLastUpdate(QSqlDatabase *db) {
 
 // --------------------------------------------------------------------
 
-void DatabaseUpdater::update0000(QSqlDatabase *db) {
+bool DatabaseUpdater::update0000(QSqlDatabase *db) {
 	QSqlQuery query(*db);
 	query.exec(
 		" CREATE TABLE IF NOT EXISTS `backend_dbupdates` ( "
@@ -154,55 +122,201 @@ void DatabaseUpdater::update0000(QSqlDatabase *db) {
 		" ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;"
 	);
 	insertUpdate(db, "new table backend_dbupdates", "u0000");
+	return true;
 }
 
 // --------------------------------------------------------------------
 
-void DatabaseUpdater::update0001(QSqlDatabase *db) {
-	QSqlQuery query(*db);
-	query.exec(
-		" CREATE TABLE IF NOT EXISTS `backend_users` ( "
-		" `id` int(11) NOT NULL AUTO_INCREMENT, "
-		" `uuid` varchar(255) DEFAULT NULL, "
-		" `name` varchar(255) DEFAULT NULL,"
-		" `password` varchar(255) DEFAULT NULL, "
-		" `role` varchar(255) DEFAULT NULL,"
-		" `dt_create` datetime DEFAULT NULL, "
-		" `dt_last_logon` datetime DEFAULT NULL, "
-		" PRIMARY KEY (`id`)"
-		" ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;"
-	);
-	insertUpdate(db, "new table backend_users", "u0001");
-}
+class Update0001 : IUpdate {
+	public:
+		virtual QString fromVersion() { return "u0000"; }
+		virtual QString toVersion()   { return "u0001"; }
+		virtual QString text() { return "new table backend_users"; }			
+		virtual bool update(QSqlDatabase *db) {
+			QSqlQuery query(*db);
+			bool bResult = query.exec(
+				" CREATE TABLE IF NOT EXISTS `backend_users` ( "
+				" `id` int(11) NOT NULL AUTO_INCREMENT, "
+				" `uuid` varchar(255) DEFAULT NULL, "
+				" `name` varchar(255) DEFAULT NULL,"
+				" `password` varchar(255) DEFAULT NULL, "
+				" `role` varchar(255) DEFAULT NULL,"
+				" `dt_create` datetime DEFAULT NULL, "
+				" `dt_last_logon` datetime DEFAULT NULL, "
+				" PRIMARY KEY (`id`)"
+				" ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;"
+			);
+			if (!bResult) {
+				std::cerr << "[ERROR] " << query.lastError().text().toStdString() << "\n";
+			}
+			return bResult;
+		}
+};
 
 // --------------------------------------------------------------------
 
-void DatabaseUpdater::update0002(QSqlDatabase *db) {
-	QSqlQuery query(*db);
-	query.prepare("INSERT INTO backend_users (uuid, name, password, role, dt_create, dt_last_logon) VALUES (:uuid, :name, :password, :role, NOW(), NOW())");
-	QString uuid = QUuid::createUuid().toString();
-	uuid = uuid.mid(1,uuid.length()-2);
-	query.bindValue(":uuid", uuid);
-	query.bindValue(":name", QString("admin"));
-	query.bindValue(":password", QString(QCryptographicHash::hash(QString("admin").toUtf8(),QCryptographicHash::Md5).toHex()));
-	query.bindValue(":role", QString("admin"));
-	query.exec();
-	insertUpdate(db, "added user admin with password admin", "u0002");
-}
+class Update0002 : IUpdate {
+	public:
+		virtual QString fromVersion() { return "u0001"; }
+		virtual QString toVersion()   { return "u0002"; }		
+		virtual QString text() { return "added user admin with password admin"; }			
+		virtual bool update(QSqlDatabase *db) {
+			QSqlQuery query(*db);
+			query.prepare("INSERT INTO backend_users (uuid, name, password, role, dt_create, dt_last_logon) VALUES (:uuid, :name, :password, :role, NOW(), NOW())");
+			QString uuid = QUuid::createUuid().toString();
+			uuid = uuid.mid(1,uuid.length()-2);
+			query.bindValue(":uuid", uuid);
+			query.bindValue(":name", QString("admin"));
+			query.bindValue(":password", QString(QCryptographicHash::hash(QString("admin").toUtf8(),QCryptographicHash::Md5).toHex()));
+			query.bindValue(":role", QString("admin"));
+			bool bResult = query.exec();
+			if (!bResult) {
+				std::cerr << "[ERROR] " << query.lastError().text().toStdString() << "\n";
+			}
+			return bResult;
+		}
+};
 
 // --------------------------------------------------------------------
 
-void DatabaseUpdater::update0003(QSqlDatabase *db) {
-	QSqlQuery query(*db);
-	query.exec(
-		" CREATE TABLE IF NOT EXISTS `backend_users_tokens` ( "
-		" `token` varchar(255) NOT NULL,"
-		" `status` varchar(255) NOT NULL, "
-		" `data` varchar(4048) NOT NULL, "
-		" `dt_start` datetime NOT NULL, "
-		" `dt_end` datetime NOT NULL, "
-		" PRIMARY KEY (`token`) "
-		" ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;"
-	);
-	insertUpdate(db, "new table backend_users", "u0003");
-}
+class Update0003 : IUpdate {
+	public:
+		virtual QString fromVersion() { return "u0002"; }
+		virtual QString toVersion()   { return "u0003"; }		
+		virtual QString text() { return "new table backend_users"; }			
+		virtual bool update(QSqlDatabase *db) {
+			QSqlQuery query(*db);
+			bool bResult = query.exec(
+				" CREATE TABLE IF NOT EXISTS `backend_users_tokens` ( "
+				" `token` varchar(255) NOT NULL,"
+				" `status` varchar(255) NOT NULL, "
+				" `data` varchar(4048) NOT NULL, "
+				" `dt_start` datetime NOT NULL, "
+				" `dt_end` datetime NOT NULL, "
+				" PRIMARY KEY (`token`) "
+				" ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;"
+			);
+			if (!bResult) {
+				std::cerr << "[ERROR] " << query.lastError().text().toStdString() << "\n";
+			}
+			return bResult;
+		}
+};
+
+// --------------------------------------------------------------------
+
+class Update0004 : IUpdate {
+	public:
+		virtual QString fromVersion() { return "u0003"; }
+		virtual QString toVersion()   { return "u0004"; }		
+		virtual QString text() { return "new table backend_teams"; }			
+		virtual bool update(QSqlDatabase *db) {
+			QSqlQuery query(*db);
+			bool bResult = query.exec(
+				" CREATE TABLE IF NOT EXISTS `backend_teams` ( "
+				" `id` int(11) NOT NULL AUTO_INCREMENT, "
+				" `name` varchar(255) NOT NULL, "
+				" `logo` varchar(255) NOT NULL, "
+				" `description` varchar(4096) NOT NULL, "
+				" `ipserver` varchar(255) NOT NULL, "
+				" PRIMARY KEY (`id`) "
+				" ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;"
+			);
+			if (!bResult) {
+				std::cerr << "[ERROR] " << query.lastError().text().toStdString() << "\n";
+			}
+			return bResult;
+		}
+};
+
+// --------------------------------------------------------------------
+
+class Update0005 : IUpdate {
+	public:
+		virtual QString fromVersion() { return "u0004"; }
+		virtual QString toVersion()   { return "u0005"; }		
+		virtual QString text() { return "new table backend_services"; }			
+		virtual bool update(QSqlDatabase *db) {
+			QSqlQuery query(*db);
+			bool bResult = query.exec(
+				" CREATE TABLE IF NOT EXISTS `backend_services` ( "
+				" `id` int(11) NOT NULL AUTO_INCREMENT, "
+				" `gameid` int(11) NOT NULL, "
+				" `name` varchar(255) NOT NULL, "
+				" `script` varchar(1024) NOT NULL, "
+				" PRIMARY KEY (`id`) "
+				" ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;"
+			);
+			if (!bResult) {
+				std::cerr << "[ERROR] " << query.lastError().text().toStdString() << "\n";
+			}
+			return bResult;
+		}
+};
+
+// --------------------------------------------------------------------
+
+class Update0006 : IUpdate {
+	public:
+		virtual QString fromVersion() { return "u0005"; }
+		virtual QString toVersion()   { return "u0006"; }		
+		virtual QString text() { return "new table backend_games"; }			
+		virtual bool update(QSqlDatabase *db) {
+			QSqlQuery query(*db);
+			bool bResult = query.exec(
+				" CREATE TABLE IF NOT EXISTS `backend_games` ( "
+				" `id` int(11) NOT NULL AUTO_INCREMENT, "
+				" `title` varchar(255) NOT NULL, "
+				" `logo` varchar(255) NOT NULL, "		
+				" PRIMARY KEY (`id`) "
+				" ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;"
+			);
+			if (!bResult) {
+				std::cerr << "[ERROR] " << query.lastError().text().toStdString() << "\n";
+			}
+			return bResult;
+		}
+};
+
+// --------------------------------------------------------------------
+
+void DatabaseUpdater::update(GlobalContext *pGlobalContext) {
+	std::cout << " * Update database\n";
+	AutoDatabaseConnection autodb(pGlobalContext);
+	if (autodb.isEmpty()) {
+		std::cerr << "[ERROR] could not connect to database \n";
+		return;
+	}
+
+	int nInstalledUpdates = 0;
+	if ( !autodb.db()->tables().contains( QLatin1String("backend_dbupdates"))) {
+		update0000(autodb.db());
+		nInstalledUpdates++;
+	};
+
+	QVector<IUpdate *> updates;
+
+	updates.push_back((IUpdate *) new Update0001());
+	updates.push_back((IUpdate *) new Update0002());
+	updates.push_back((IUpdate *) new Update0003());
+	updates.push_back((IUpdate *) new Update0004());
+	updates.push_back((IUpdate *) new Update0005());
+	updates.push_back((IUpdate *) new Update0006());
+
+	for (int i = 0; i < updates.size(); i++) {
+		IUpdate *upd = updates[i];
+		if (getLastUpdate(autodb.db()) == upd->fromVersion()) {
+			if (upd->update(autodb.db())) {
+				insertUpdate(autodb.db(), upd->text(), upd->toVersion());
+				nInstalledUpdates++;
+			} else {
+				std::cout << " * Problem with " << upd->toVersion().toStdString() << "\n";
+			}
+		}
+	}
+	
+	std::cout << " *** Installed " << nInstalledUpdates << " updates. Current version: " << getLastUpdate(autodb.db()).toStdString() << "\n";
+};
+
+// --------------------------------------------------------------------
+
--- backend/src/database/databaseupdater.h
+++ backend/src/database/databaseupdater.h
@@ -4,19 +4,23 @@
 #include <QSettings>
 #include "../globalcontext.h"
 
+class IUpdate {
+	public:
+		virtual QString fromVersion() = 0;
+		virtual QString toVersion() = 0;
+		virtual QString text() = 0;
+		virtual bool update(QSqlDatabase *db) = 0;
+};
+
+
 class DatabaseUpdater {
 	public:
 		void update(GlobalContext *pGlobalContext);
 		void create(GlobalContext *pGlobalContext);
-
 	private:
 		void insertUpdate(QSqlDatabase *db, QString name, QString version);
 		QString getLastUpdate(QSqlDatabase *db);
-
-		void update0000(QSqlDatabase *db);
-		void update0001(QSqlDatabase *db);
-		void update0002(QSqlDatabase *db);
-		void update0003(QSqlDatabase *db);
+		bool update0000(QSqlDatabase *db);
 };
 
 #endif // DATABASEUPDATER
