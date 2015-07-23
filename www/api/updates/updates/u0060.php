a5c04dedd8950e5baa8fe626c344788462020bac
sea-kg <mrseakg@gamail.com>
2015-07-23 16:21:36 +0600

    fixed problem with u0060

--- www/api/updates/updates/u0060.php
+++ www/api/updates/updates/u0060.php
@@ -10,13 +10,13 @@ function update_u0060($conn) {
 	$stmt_users_quests = $conn->prepare('SELECT idquest
 		FROM quest
 		LEFT JOIN users_quests ON users_quests.questid = quest.idquest
-		WHERE for_person <>0
+		WHERE for_person <> 0
 		AND isnull( dt_passed )'
 	);
 	$stmt_users_quests->execute();
 	while ($row = $stmt_users_quests->fetch()) {
 		$questid = intval($row['idquest']);
-		$conn->prepare('DELETE FROM quest WHERE idquest')->execute(array($questid));
+		$conn->prepare('DELETE FROM quest WHERE idquest = ?')->execute(array($questid));
 	}
 	return true;
 }
