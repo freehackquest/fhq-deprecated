d91679ea01621e4fc0a5417eb6ab86757e16ec81
sea-kg <mrseakg@gamail.com>
2015-10-31 22:27:21 +0600

    changed github place repo

--- www/copyright.php
+++ www/copyright.php
@@ -1,5 +1,5 @@
 <?php
 echo '<center>
-	<font face="Arial" size=2>Copyright © 2011-2015 sea-kg. Source code: <a href="https://github.com/sea-kg/fhq">github.com</a> API: <a href="api/?html">html</a> or <a href="api/?json">json</a> VM: <a href="http://files.sea-kg.com/fhq-ova/" target="_ablank">ova</a> Team: <a href="https://ctftime.org/team/16804">ctftime</a> Donate: <a href="donate.html">donate</a><br></font>
+	<font face="Arial" size=2>Copyright © 2011-2015 sea-kg. Source code: <a href="https://github.com/freehackquest/fhq">github.com</a> API: <a href="api/?html">html</a> or <a href="api/?json">json</a> VM: <a href="http://files.sea-kg.com/fhq-ova/" target="_ablank">ova</a> Team: <a href="https://ctftime.org/team/16804">ctftime</a> Donate: <a href="donate.html">donate</a><br></font>
 	<!-- pre><div id="debug_info"/></pre -->
 </center>';
